95ff3705dfe6961aa74b8e61b4ae69efff34155a
300155313 <Aaron.Creor@student.ufv.ca>
2024-03-29 18:14:36 -0700

    repository

--- app/src/main/java/com/example/stockmarketshowdown/Repository.kt
+++ app/src/main/java/com/example/stockmarketshowdown/Repository.kt
@@ -1,23 +1,29 @@
-import com.google.gson.Gson
-import java.net.URL
-
-data class Company(val description: String, val displaySymbol: String)
+data class Company(
+    val currency: String,
+    val description: String,
+    val displaySymbol: String,
+    val figi: String?,
+    val isin: String?,
+    val mic: String,
+    val shareClassFIGI: String,
+    val symbol: String,
+    val symbol2: String,
+    val type: String)
 
 class Repository {
     // This function fetches the list of publicly traded US companies from the specified API
-    private fun fetchCompanyList(): List<Company> {
-        val url = URL("https://finnhub.io/api/v1/stock/symbol?exchange=US&mic=XNYS&token=co3lv89r01qj6vn86qd0co3lv89r01qj6vn86qdg") // API endpoint
-        val json = url.readText()
-
-        // Assuming the API response is a JSON array where each object contains description and displaySymbol fields
-        return Gson().fromJson(json, Array<Company>::class.java).toList()
-    }
-
-    // This is where you would initialize the repository with the fetched company list
-    private val stockResources: List<Company> = fetchCompanyList()
-
-    // You may expose a method to retrieve the list of companies if needed
-    fun getCompanies(): List<Company> {
-        return stockResources
-    }
+    private var companyResources = listOf(
+        Company  (
+                "USD",
+                "REPUBLIC SERVICES INC",
+                "RSG",
+                "BBG000BPXVJ6",
+                null,
+                "XNYS",
+                "BBG001S9DL33",
+                "RSG",
+                "",
+                "Common Stock"
+        )
+    )
 }
