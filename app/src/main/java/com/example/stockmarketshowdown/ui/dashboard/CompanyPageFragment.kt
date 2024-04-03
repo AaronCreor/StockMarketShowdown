c4e3784abac6972974d9f341bb62523495e97fdf
300155313 <Aaron.Creor@student.ufv.ca>
2024-04-02 22:22:36 -0700

    RV on click takes us to page

--- app/src/main/java/com/example/stockmarketshowdown/ui/dashboard/CompanyPageFragment.kt
+++ app/src/main/java/com/example/stockmarketshowdown/ui/dashboard/CompanyPageFragment.kt
@@ -5,13 +5,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.stockmarketshowdown.MainViewModel
 import com.example.stockmarketshowdown.databinding.FragmentCompanypageBinding
 
 class CompanyPageFragment : Fragment() {
 
     private var _binding: FragmentCompanypageBinding? = null
     private val binding get() = _binding!!
-
+    private lateinit var viewModel: MainViewModel
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -23,8 +25,12 @@ class CompanyPageFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        // Initialize UI components and set listeners here
+        viewModel = ViewModelProvider(requireActivity()).get(MainViewModel::class.java)
+        val index = arguments?.getInt("companyIndex") ?: -1
+        if (index != -1) {
+            val companyDescription = viewModel.getCurrentCompanyInfo(index)
+                binding.textHome.text = companyDescription
+        }
     }
 
     override fun onDestroyView() {
